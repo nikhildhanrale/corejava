257642cf7ecdd39a63e6952e8e77e0ad5b76729e
nikhil <nikhild.shantiinfosoft@gmail.com>
2023-07-10 12:50:48 +0530

    this first git commit

---
+++ Java/Core_java/Demo.java
@@ -0,0 +1,12 @@
+ package Java.Core_java;
+ 
+ /**
+  * Demo
+  */
+ public class Demo {
+ public static void main(String[] args) {
+    System.out.println("There is will there is way to become good programmer");
+ }
+    
+ }
+    
