f18279a6612f00cbd7f4597fa27b241d31207b4c
nikhil <nikhild.shantiinfosoft@gmail.com>
2023-07-19 10:49:34 +0530

    abd

--- Java/Core_java/Simple.java
+++ Java/Core_java/Simple.java
@@ -2,4 +2,12 @@ package corejava.Java.Core_java;
 
 public class Simple {
     
+    public static void main(String[] args) {
+        for(int i=1; i<=10;i++)
+            System.out.println("tell me about this");
+           // System.out.println("This is me : "+i);
+            System.out.println("Wow great");
+    
+    }
+    
 }
---
+++ Java/Core_java/Stacktrace2.java
@@ -0,0 +1,15 @@
+package corejava.Java.Core_java;
+
+public class Stacktrace2 {
+    public static void main(String[] args) {
+        int a=5;
+        int b=0;
+        try{
+            System.out.println(a/b);
+        }
+        catch (ArithmeticException e) {
+            System.out.println(e.toString());
+        }
+    }
+    
+}
